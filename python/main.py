c50741f082c105cdac076b5fad5fef9945ac6202
Osamu-kj <kevin.jerebica@gmail.com>
2022-07-14 18:26:54 +0200

    Working on the python demo...

--- python/main.py
+++ python/main.py
@@ -13,6 +13,7 @@ sys.path.insert(0, ".")
 
 # importing local files 
 import tokens.tokens
+import tokens.typetokens
 
 # exceptions 
 class InvalidExpression(Exception):
@@ -65,22 +66,43 @@ class ProgramSettings():
     }
 
     symbols = {
-        "+": {},
-        "-": {},
-        "*": {},
-        "/": {},
-        "(": {},
-        ")": {},
-        " ": {},
+        "+": {
+            "privilege": 3,
+            "label": "operator"
+        },
+        "-": {
+            "privilege": 3,
+            "label": "operator"
+        },
+        "*": {
+            "privilege": 2,
+            "label": "operator"
+        },
+        "/": {
+            "privilege": 2,
+            "label": "operator"
+        },
+        "(": {
+            "privilege": 5,
+            "label": "privilege_operator"
+        },
+        ")": {
+            "privilege": 5,
+            "label": "privilege_operator"
+        },
     }
 
     def __init__ (self): 
         pass 
 
     @staticmethod
-    def checkExpression(expression): 
+    def check_expession(expression): 
         for char in expression: 
             
+            # check if its a empty space 
+            if (not char.strip()): 
+                continue 
+
             # check if its a number 
             if (char in ProgramSettings.numbers): 
                 continue 
@@ -99,25 +121,74 @@ class ProgramSettings():
         return True 
 
     @staticmethod 
-    def generateTokensArray(expression): 
+    def generate_tokens_array(expression): 
         tokens_array = []
+        
+        numbers = "".join(ProgramSettings.numbers.keys())
+        vars = "".join(ProgramSettings.vars.keys())
+        symbols = "".join(["\\" + symbol for symbol in ProgramSettings.symbols.keys()])
 
-        tokens_array = re.split("([1234]+)|([6]+)", expression) 
+        regex = f"(?:[{numbers}]+)|(?:[{vars}]+)|(?:[{symbols}])"
+
+        tokens_array = re.findall(regex, expression)
 
         return tokens_array
 
     @staticmethod
-    def scanTokens(expression): 
+    def find_token_type(token): 
+        if (token.isnumeric()):
+            return "num"
+        
+        if (token in ProgramSettings.vars):
+            return "var"
+        
+        if (token in ProgramSettings.symbols): 
+            return "symbol"
+        
+        raise InvalidExpression(f"Invalid token type {str(token)}")
+
+    @staticmethod 
+    def generate_type_tokens_array(tokens_array): 
+        type_tokens_array = [] 
+
+        for token in tokens_array: 
+            type_token = None
+            if (token.isnumeric()):
+                number = int(token)
+                type_token = tokens.typetokens.Number(number, 1) 
+            elif (token.isalpha() and token in ProgramSettings.vars):
+                type_token = tokens.typetokens.Variable(token, 1)
+            elif (token in ProgramSettings.symbols):
+                privilege = ProgramSettings.symbols[token]["privilege"]
+                label = ProgramSettings.symbols[token]["label"]
+                type_token = tokens.typetokens.Symbol(token, label, privilege)
+
+            type_tokens_array.append(type_token)
+
+        return type_tokens_array 
+
+    @staticmethod 
+    def check_token_rules(tokens_array): 
+        return True
+
+    @staticmethod
+    def scan_tokens(expression): 
 
         tokens_array = privileges_array = levels_array = []
 
-        if (not ProgramSettings.checkExpression(expression)): 
+        if (not ProgramSettings.check_expession(expression)): 
             raise InvalidExpression("Given expression is invalid")
 
-        tokens_array = ProgramSettings.generateTokensArray(expression)
+        tokens_array = ProgramSettings.generate_tokens_array(expression)
 
         if (not len(tokens_array)):
             raise InvalidExpression("Expression does not follow the expression rules")
+
+        tokens_array = ProgramSettings.generate_type_tokens_array(tokens_array) 
+
+        if (not ProgramSettings.check_token_rules(tokens_array)):
+            raise InvalidExpression("Given expression is invalid") 
+        
 
 
         return [tokens_array, privileges_array, levels_array]
@@ -127,7 +198,7 @@ def main():
     expression = str(expression).strip()
     
     try: 
-        tokens, privileges, levels = ProgramSettings.scanTokens(expression)
+        tokens, privileges, levels = ProgramSettings.scan_tokens(expression)
         
         print ("Tokens -> " + str(tokens)) 
         print ("Privileges -> " + str(privileges)) 
---
+++ python/tokens/typetokens.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3 
+
+import os 
+import sys 
+
+class TypeToken: 
+    def __init__(self, value, privilege ): 
+        self.value = value
+        self.privilege = privilege
+
+    def __str__ (self): 
+        return str(self.value)
+
+class Number (TypeToken): 
+    pass 
+
+class Variable (TypeToken): 
+    pass 
+
+class Symbol (TypeToken): 
+    def __init__ (self, value, label, privilege): 
+        self.value = value 
+        self.label = label 
+        self.privilege = privilege
